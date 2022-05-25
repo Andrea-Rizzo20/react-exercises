125360504f0e592f616608589f4579aca7a80e8d
Andrea Rizzo <r.andrea2093@gmail.com>
2022-05-25 18:46:03 +0200

    Esercizio/Components-9

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Hello } from "./Hello";
+import { Message } from "./Message";
 
 export class App extends React.Component {
   render() {
@@ -7,9 +8,11 @@ export class App extends React.Component {
       <div>
         <Hello />
         <Hello />
+        <Message />
       </div>
     );
   }
 }
 
 // Yes, we can use the Hello component more than once
+// Yes, we can render the component Message directly to the App
