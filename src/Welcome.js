4c9999391725a5fd774a3dd90aa1be3c415a5051
Andrea Rizzo <r.andrea2093@gmail.com>
2022-05-25 13:27:35 +0200

    Esercizio/props-03

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,13 +1,18 @@
 import React from "react";
 
-export class Welcome extends React.Component{
-    render(){
-        return <p>Welcome, {this.props.name}!</p>
-    }
+export class Welcome extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome, {this.props.name}!</p>
+        <p>Your age is {this.props.age}</p>
+      </div>
+    );
+  }
 }
 
 // Yes!, we can pass default props value
 
 Welcome.defaultProps = {
-    name: 'User',
-}
+  name: "User",
+};
