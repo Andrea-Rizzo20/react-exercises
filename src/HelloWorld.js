0cb75e06cb850ae7e7e2d6d186632ad04329c0af
Andrea Rizzo <r.andrea2093@gmail.com>
2022-05-25 11:01:09 +0200

    Esercizio/jsx-4

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -2,4 +2,7 @@ const hello = <h1>Hello, World!</h1>;
 
 const sayHello = name => <h1>Hello, {name}</h1>;
 
-//  if the name variable is not passed to the function, the tag h1 will render only Hello,
+// if the name variable is not passed to the function, the tag h1 will render only Hello,
+
+// if the name variable contains a JSX expression instead of a string, we will render the result of the expression
+// example: a = 10, b = 18, num = a + b,  <h1> Result {num} </h1>, this will render a h1 contain Result 28
