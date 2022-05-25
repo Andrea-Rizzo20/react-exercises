23bcf8ce9e68592a138171b767d7ddf8a2cf553f
Andrea Rizzo <r.andrea2093@gmail.com>
2022-05-25 11:00:46 +0200

    Esercizio/jsx-3

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,3 +1,5 @@
 const hello = <h1>Hello, World!</h1>;
 
 const sayHello = name => <h1>Hello, {name}</h1>;
+
+//  if the name variable is not passed to the function, the tag h1 will render only Hello,
