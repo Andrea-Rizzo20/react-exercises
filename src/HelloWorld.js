0eeeed21f142c4db20bfb7d877f099da260c3cc1
Andrea Rizzo <r.andrea2093@gmail.com>
2022-05-25 11:06:21 +0200

    Esercizio/jsx-5

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -6,3 +6,5 @@ const sayHello = name => <h1>Hello, {name}</h1>;
 
 // if the name variable contains a JSX expression instead of a string, we will render the result of the expression
 // example: a = 10, b = 18, num = a + b,  <h1> Result {num} </h1>, this will render a h1 contain Result 28
+
+const sum = (a,b) => <h2>{a+b}</h2>
