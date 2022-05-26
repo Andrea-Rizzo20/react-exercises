596b18ad12eda101553a616fa873b4c9a451142d
Andrea Rizzo <r.andrea2093@gmail.com>
2022-05-26 15:44:23 +0200

    Esercizio/Events-04

--- src/ClickTracker.js
+++ src/ClickTracker.js
@@ -1,30 +1,38 @@
 import React from "react";
+import logo from "./logo192.png";
 
-export class ClickTracker extends React.Component{
-    state ={
-        button : "Nessun Bottone"
-    }
+export class ClickTracker extends React.Component {
+  state = {
+    button: "Nessun Bottone",
+  };
 
-    clickTrackerHandle = (event) =>{
-       this.setState((state) =>{
-           return {
-               button: event.target.name
-           }
-       })
-    }
+  clickTrackerHandle = (event) => {
+    let nameBtn = event.currentTarget.name;
+    this.setState((state) => {
+      return {
+        button: nameBtn,
+      };
+    });
+  };
 
-    render(){
-        return(
-            <div>
-                <button name="Primo" onClick={this.clickTrackerHandle}>Primo</button>
-                <button name="Secondo" onClick={this.clickTrackerHandle}>Secondo</button>
-                <button name="Terzo" onClick={this.clickTrackerHandle}>Terzo</button>
-                <h1>Ti ho visto premere il bottone : {this.state.button}</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <button name="Primo" onClick={this.clickTrackerHandle}>
+          <img src={logo} alt="logo"></img>
+        </button>
+        <button name="Secondo" onClick={this.clickTrackerHandle}>
+          Secondo
+        </button>
+        <button name="Terzo" onClick={this.clickTrackerHandle}>
+          Terzo
+        </button>
+        <h1>Ti ho visto premere il bottone : {this.state.button}</h1>
+      </div>
+    );
+  }
 }
 
-ClickTracker.defaultProps ={
-    button : "Nessun Bottone"
-}
+ClickTracker.defaultProps = {
+  button: "Nessun Bottone",
+};
