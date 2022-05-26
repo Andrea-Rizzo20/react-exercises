15593246abbee7708e56a8d7bc786a38fea9e168
Andrea Rizzo <r.andrea2093@gmail.com>
2022-05-26 10:45:33 +0200

    Esercizio/LifeCycle-01

--- src/Counter.js
+++ src/Counter.js
@@ -6,22 +6,31 @@ export class Counter extends React.Component {
     count: this.props.initialValue ?? 0,
   };
 
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    const countInterval = setInterval(() => {
-      this.setState((state) => {
-        if (state.count < this.props.initialValue * 10) {
-          return { count: state.count + this.props.incrementAmount ?? 1 };
-        } else {
-          clearInterval(countInterval)
-          return { count: this.props.initialValue };
-        }
-      });
-    }, this.props.incrementInterval ?? 1000);
-  }
+  //   const countInterval = setInterval(() => {
+  //     this.setState((state) => {
+  //       if (state.count < this.props.initialValue * 10) {
+  //         return { count: state.count + this.props.incrementAmount ?? 1 };
+  //       } else {
+  //         clearInterval(countInterval)
+  //         return { count: this.props.initialValue };
+  //       }
+  //     });
+  //   }, this.props.incrementInterval ?? 1000);
+  // }
+
   render() {
     return <CounterDisplay count={this.state.count} />;
+  }
+
+  componentDidMount() {
+    const countInterval = setInterval(() => {
+      this.setState((state) => {
+        return { count: state.count + this.props.incrementAmount ?? 1 };
+      });
+    }, this.props.incrementInterval ?? 1000);
   }
 }
 
