7c8abcbf5713d04e34fa0cc6ab65fa067c7acf2c
Andrea Rizzo <r.andrea2093@gmail.com>
2022-06-03 13:07:15 +0200

    Esercizio Component-composition-02

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export class App extends React.Component{
     }
     render(){
         return (
-            <Container>
+            <Container title='My App'>
                 <Welcome />
                 <ClickCounter />
                 <ClickTracker />
