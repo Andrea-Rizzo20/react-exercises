3b40135b51553239c00578ab65edf16184b66a47
Andrea Rizzo <r.andrea2093@gmail.com>
2022-06-06 11:18:34 +0200

    Esercizio/Context-01

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { DisplayLanguage } from "./DisplayLanguage";
+import { LanguageContext } from "./LanguageContext";
+
+export class App extends React.Component{
+    state={
+        language: 'en'
+    }
+
+    languageChangeHandle = event =>{
+        this.setState({
+            language: event.target.value
+        })
+    }
+    render(){
+        return(
+            <div>
+                <select value={this.state.language} onChange={this.languageChangeHandle}>
+                    <option value='en'>English</option>
+                    <option value='it'>Italiano</option>
+                </select>
+                <LanguageContext.Provider value={this.state.language}>
+                    <DisplayLanguage />
+                </LanguageContext.Provider>
+            </div>
+        )
+    }
+}
