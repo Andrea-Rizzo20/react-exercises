550ec9cc3c23d6d038df98d445554c4458f6e3c8
Andrea Rizzo <r.andrea2093@gmail.com>
2022-06-06 11:30:09 +0200

    Esercizio/Context-02

--- src/DisplayLanguage.js
+++ src/DisplayLanguage.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { LanguageContext } from "./LanguageContext";
 
 export class DisplayLanguage extends React.Component {
+
   render() {
-    return <div></div>;
+      const textLang ={
+          en:{text:'Your choise is: English'},
+          it:{text: 'La tua scelta è Italiano'},
+      }
+    return(
+        <LanguageContext.Consumer>
+            {language =>{
+                return (
+                    <h1>{textLang[language].text}</h1>
+                )
+            }}
+        </LanguageContext.Consumer>
+    )
   }
 }
