83dfb7fcca62d291f2356242138c218b945638ad
Andrea Rizzo <r.andrea2093@gmail.com>
2022-06-07 11:42:37 +0200

    Esercizio/Function-component-03

--- src/Sum.js
+++ src/Sum.js
@@ -1,3 +1,5 @@
 export function Sum( { numbers = [0,1] } ){
     return <h1>La somma è {numbers.reduce((a,b)=> a + b)}</h1>
 }
+
+// if numbers  props  of Sum components is not set, the app will broke, we can set the default value of a props by explode the props of Sum component, and set it
