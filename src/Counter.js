097c8399a3ea35f4d58c3b041182ab39811c74d9
Andrea Rizzo <r.andrea2093@gmail.com>
2022-06-08 11:12:50 +0200

    Esercizio/useEffect-01

--- src/Counter.js
+++ src/Counter.js
@@ -1,8 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Counter = ({ initialValue = 0, incrementValue = 1 }) => {
 
     const [counter, setCounter] = useState(initialValue)
+
+    const onCounterChange = value => console.log(value)
+
+    useEffect(() =>{
+        onCounterChange(counter)
+    },[counter])
+
+
     const incrementHandle = () =>{
         setCounter(counter => counter + incrementValue)
     }
