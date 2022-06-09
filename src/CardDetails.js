c207e995a4512f368c7412e4972d4ce383dd2a4f
Andrea Rizzo <r.andrea2093@gmail.com>
2022-06-09 15:23:52 +0200

    Esercizio/useRef-02

--- src/CardDetails.js
+++ src/CardDetails.js
@@ -2,15 +2,23 @@ import { useEffect, useRef } from "react"
 
 const CardDetails = ({initialData = {model:'Lamborghini',year:'2018', color:'black'}}) => {
 
+    const {model,year, color} = initialData
 
-    const formRef = useRef(initialData)
+    const formRef = useRef(null)
+
+    useEffect(()=>{
+        formRef.current.reset()
+    },[initialData])
+
+
+
 
 
     return(
         <form ref={formRef}>
-            <input name="model" defaultValue={formRef.model}></input>
-            <input name="year" defaultValue={formRef.year}></input>
-            <input name="color" defaultValue={formRef.color}></input>
+            <input name="model" defaultValue={model}></input>
+            <input name="year" defaultValue={year}></input>
+            <input name="color" defaultValue={color}></input>
         </form>
 
     )
