4b9c95daa9e1b7685fe54d4f06ac0fa21351312c
Andrea Rizzo <r.andrea2093@gmail.com>
2022-06-12 16:16:01 +0200

    Esercizio React-router-04

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import { Routes, Route } from 'react-router-dom'
-import Counter from './Counter'
-import ShowGitHubUser from './ShowGitHubUser'
-import { Welcome } from './Welcome'
+import { Routes, Route, Link } from "react-router-dom";
+import Counter from "./Counter";
+import ShowGitHubUser from "./ShowGitHubUser";
+import { Welcome } from "./Welcome";
 
-const App = () =>{
-    return(
-        <Routes>
-            <Route path='/' element={<Welcome name="Andrea"/>}/>
-            <Route path='/Counter' element={<Counter/>}/>
-            <Route path='/users/:username' element={<ShowGitHubUser/>}/>
-        </Routes>
-    )
-}
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Welcome name="Andrea" />} />
+        <Route path="/Counter" element={<Counter />} />
+        <Route path="/users/:username" element={<ShowGitHubUser />} />
+      </Routes>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/Counter">Counter</Link>
+        <Link to="/users/Andrea-Rizzo20">Andrea Rizzo GitHub</Link>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
