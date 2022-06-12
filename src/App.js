7357ba5ea951095450970cabbce81f99ac7b7e20
Andrea Rizzo <r.andrea2093@gmail.com>
2022-06-12 16:33:25 +0200

    Esercizio React-router-05

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const App = () => {
         <Route path="/" element={<Welcome name="Andrea" />} />
         <Route path="/Counter" element={<Counter />} />
         <Route path="/users/:username" element={<ShowGitHubUser />} />
+        <Route path="*" element={<div><h1>NO Joke Man, go back this page don't exist</h1></div>}/>
       </Routes>
       <div>
         <Link to="/">Home</Link>
