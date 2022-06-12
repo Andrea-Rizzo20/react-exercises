07daa32e7af83149c5659ed40175e708893264e9
Andrea Rizzo <r.andrea2093@gmail.com>
2022-06-12 15:09:57 +0200

    Esercizio React-router-01

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom'
+import { Welcome } from './Welcome'
+
+const App = () =>{
+    return(
+        <Routes>
+            <Route path='/' element={<Welcome name="Andrea"/>}/>
+        </Routes>
+    )
+}
+
+export default App
